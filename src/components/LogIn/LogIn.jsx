9d9c8e2435ac21be40d751c30da7109a9ae52d2c
Alexey Svistunov <xdayx53@gmail.com>
2024-03-04 09:53:06 +0300

    add login functionality

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Auth from '../Auth/Auth'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import ROUTES from '../../utils/routes';
+import { setUser } from '../../store/slices/userSlice';
+
 
 const LogIn = () => {
 
-  const onClickAuth = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const onClickAuth = (email, password) => {
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userDetails) => {
+        console.log(userDetails);
+        dispatch(setUser({email}))
+        navigate(ROUTES.LANDINGPAGE)
+      })
+
+      .catch((error) => {
+        console.log(error);
+      })
 
   }
+
+
   return (
     <>
       <Auth propWord={'Log In'} onClickAuth={onClickAuth}/>
