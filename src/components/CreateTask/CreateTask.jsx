49364b4ac0f4a1c8c277253af09d563583f6abe8
Alexey Svistunov <xdayx53@gmail.com>
2024-03-09 19:03:28 +0300

    add nothing here yet if there's no items and styles for calendar

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -15,7 +15,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import "./CreateTask.css";
 
-const CreateTask = ({ currentDay, showListHandler}) => {
+const CreateTask = ({ currentDay, showListHandler }) => {
   const dispatch = useDispatch();
 
   const [userData, setUserData] = useState([]);
@@ -23,7 +23,7 @@ const CreateTask = ({ currentDay, showListHandler}) => {
 
   const [title, setTitle] = useState("");
   const [descr, setDescr] = useState("");
-  const [currentColor, setCurrentColor] = useState("Фисташковый")
+  const [currentColor, setCurrentColor] = useState(["Фисташковый", "#EDEAEA"]);
 
   const { email } = useAuth();
 
@@ -62,10 +62,8 @@ const CreateTask = ({ currentDay, showListHandler}) => {
   };
 
   const setColor = (color) => {
-    setCurrentColor(color)
-  }
-
-
+    setCurrentColor(color);
+  };
 
   return (
     <section className="new-task">
@@ -92,9 +90,19 @@ const CreateTask = ({ currentDay, showListHandler}) => {
         </div>
       </div>
 
-      <h3 className="new-task__color-title">Card Color</h3>
+      <div className="new-task__color-picking">
+        <h3 className="new-task__color-title">Card Color</h3>
+        <div className="new-task__color-picked">Your picked color is:  <span>{currentColor[0]}</span></div>
+      </div>
       <div className="new-task__colors">
-        <button className={currentColor[0] === 'Фисташковый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Фисташковый', '#ADF7B6'])}>
+        <button
+          className={
+            currentColor[0] === "Фисташковый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Фисташковый", "#ADF7B6"])}
+        >
           <svg
             width="52"
             height="52"
@@ -114,7 +122,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Фиолетово-Розовый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Фиолетово-Розовый', '#A817C0'])}>
+        <button
+          className={
+            currentColor[0] === "Фиолетово-Розовый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Фиолетово-Розовый", "#A817C0"])}
+        >
           <svg
             width="52"
             height="52"
@@ -134,7 +149,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Бежевый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Бежевый', '#FFC09F'])}>
+        <button
+          className={
+            currentColor[0] === "Бежевый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Бежевый", "#FFC09F"])}
+        >
           <svg
             width="52"
             height="52"
@@ -154,7 +176,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Голубой' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Голубой', '#B0FFFA'])}>
+        <button
+          className={
+            currentColor[0] === "Голубой"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Голубой", "#B0FFFA"])}
+        >
           <svg
             width="52"
             height="52"
@@ -174,7 +203,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Желтый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Желтый', '#FCFF52'])}>
+        <button
+          className={
+            currentColor[0] === "Желтый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Желтый", "#FCFF52"])}
+        >
           <svg
             width="52"
             height="52"
@@ -194,7 +230,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button  className={currentColor[0] === 'Зеленый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Зеленый', '#4EFF31'])}>
+        <button
+          className={
+            currentColor[0] === "Зеленый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Зеленый", "#4EFF31"])}
+        >
           <svg
             width="52"
             height="52"
@@ -213,7 +256,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Бирюзовый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Бирюзовый', '#5BFFD8'])}>
+        <button
+          className={
+            currentColor[0] === "Бирюзовый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Бирюзовый", "#5BFFD8"])}
+        >
           <svg
             width="52"
             height="52"
@@ -233,7 +283,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Синий' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Синий', '#0038FF'])}>
+        <button
+          className={
+            currentColor[0] === "Синий"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Синий", "#0038FF"])}
+        >
           <svg
             width="52"
             height="52"
@@ -253,7 +310,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Фиолетовый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Фиолетовый', '#622BFF'])}>
+        <button
+          className={
+            currentColor[0] === "Фиолетовый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Фиолетовый", "#622BFF"])}
+        >
           <svg
             width="52"
             height="52"
@@ -273,7 +337,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Малиновый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Малиновый', '#D21DFF'])}>
+        <button
+          className={
+            currentColor[0] === "Малиновый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Малиновый", "#D21DFF"])}
+        >
           <svg
             width="52"
             height="52"
@@ -293,7 +364,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Алый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Алый', '#B92350'])}>
+        <button
+          className={
+            currentColor[0] === "Алый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Алый", "#B92350"])}
+        >
           <svg
             width="52"
             height="52"
@@ -313,7 +391,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Красный' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Красный', '#FF0000'])}>
+        <button
+          className={
+            currentColor[0] === "Красный"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Красный", "#FF0000"])}
+        >
           <svg
             width="52"
             height="52"
@@ -332,7 +417,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Белый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Белый', '#E9E3E8'])}>
+        <button
+          className={
+            currentColor[0] === "Белый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Белый", "#E9E3E8"])}
+        >
           <svg
             width="52"
             height="52"
@@ -352,7 +444,14 @@ const CreateTask = ({ currentDay, showListHandler}) => {
           </svg>
         </button>
 
-        <button className={currentColor[0] === 'Серый' ? "new-task__color new-task__color--active" : "new-task__color"} onClick={() => setColor(['Серый', '#554E55'])}>
+        <button
+          className={
+            currentColor[0] === "Серый"
+              ? "new-task__color new-task__color--active"
+              : "new-task__color"
+          }
+          onClick={() => setColor(["Серый", "#554E55"])}
+        >
           <svg
             width="52"
             height="52"
@@ -380,7 +479,6 @@ const CreateTask = ({ currentDay, showListHandler}) => {
             className="task-tag__input"
             type="text"
             placeholder="Set a tag for your task"
-          
           ></input>
           <div className="task-tag__filters">
             <button className="task-tag__filter">Daily Routine</button>
@@ -390,15 +488,21 @@ const CreateTask = ({ currentDay, showListHandler}) => {
         </div>
       </div>
 
-      <button className="new-task__btn" onClick={() => 
-       {
-        addTask(title, descr, email, currentDay.toLocaleDateString(), currentColor[1])
-        showListHandler()
-       }
-      }>
+      <button
+        className="new-task__btn"
+        onClick={() => {
+          addTask(
+            title,
+            descr,
+            email,
+            currentDay.toLocaleDateString(),
+            currentColor[1]
+          );
+          showListHandler();
+        }}
+      >
         <span className="material-symbols-outlined">check</span>
       </button>
-
     </section>
   );
 };
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -15,17 +15,16 @@ const TaskList = ({showListHandler, currentDay}) => {
     dispatch(getTodos(email));
   }, []);
 
-
   return (
     <section className="task-list">
       <ul className="tasks-list">
-        {todosForDate &&
+        {todosForDate?.length ? 
           todosForDate.map((el) => (
             <li key={el.title} className="tasks-list__task list-task" style={{backgroundColor: el.color}}>
               <input className="list-task__checkbox" type="checkbox"></input>
               <span className='list-task__title'>{el.title}</span>
             </li>
-          ))}
+          )) : <img src="src/assets/images/nothing-here.png" style={{display: 'block', margin: '200px auto'}}></img>}
       </ul>
 
       <button className="task-list__btn" onClick={showListHandler}>
