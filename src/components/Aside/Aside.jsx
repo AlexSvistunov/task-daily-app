4eb3fac06dfc80a9706b6b7e339278ad00485af9
Alexey Svistunov <xdayx53@gmail.com>
2024-03-16 10:48:36 +0300

    add trash bin svg

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -5,8 +5,9 @@ import Calendar from "react-calendar";
 import "./Aside.css";
 
 const Aside = ({ currentDay, changeDay }) => {
-  // const todos = useSelector((state) => state.todos.todoList);
-  // const todosForDate = todos?.todos?.filter((el) => el['day'] === currentDay.toLocaleDateString())
+  const todos = useSelector((state) => state.todos.todoList);
+  const todosForDate = todos && todos.filter((el) => el['day'] === currentDay.toLocaleDateString()).length
+  console.log(todosForDate);
   return (
     <aside className="aside">
       <div className="aside__inner">
@@ -22,12 +23,13 @@ const Aside = ({ currentDay, changeDay }) => {
           <li className="task">
             <span className="task__name">For this day</span>
             <span className="task__amount">
+            {todosForDate}
               {/* {todosForDate && todosForDate.length} */}
             </span>
           </li>
         </ul>
 
-        <ul className="lists">
+        {/* <ul className="lists">
           <h3 className="lists__title">Lists</h3>
           <li className="list">
             <span className="list__title">Daily Routine</span>
@@ -38,7 +40,7 @@ const Aside = ({ currentDay, changeDay }) => {
             <span className="list__title">Study</span>
             <span className="list__amount">0</span>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </aside>
   );
--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -26,20 +26,22 @@ const TaskModal = ({
   const dispatch = useDispatch();
   async function updateTitle() {
     const db = getDatabase();
-   
-    const updates = {}
+
+    const updates = {};
 
     updates[`users/${token}/${dataModal.currentIndex}/title`] = dataModal.title;
     updates[`users/${token}/${dataModal.currentIndex}/descr`] = dataModal.descr;
 
-    update(ref(db), updates).then(() => {
-      dispatch(getTodos(token));
-      console.log('success');
-      setModalIsOpen(false)
-    }).catch((error) => console.log(error.message))
+    update(ref(db), updates)
+      .then(() => {
+        dispatch(getTodos(token));
+        console.log("success");
+        setModalIsOpen(false);
+      })
+      .catch((error) => console.log(error.message));
 
     // AND DESCR AND FIX INDEX
-    
+
     // const theDoc = doc(db, "users", email);
 
     // await updateDoc(theDoc, {
@@ -101,10 +103,103 @@ const TaskModal = ({
         >
           X
         </button>
-        <button className="task-modal__apply" onClick={() => updateTitle()}> 
-        {/* token, index, email, value */}
-          Apply
-        </button>
+
+        <div className="task-modal__btn-box">
+          <button className="task-modal__apply" onClick={() => updateTitle()}>
+            {/* token, index, email, value */}
+            Apply
+          </button>
+
+          <button className="task-modal__delete">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="35"
+              height="35"
+              viewBox="0 0 48 48"
+            >
+              <linearGradient
+                id="i9gMV8RPRiXBVRoCh9BlCa_BJkQWseLWhe4_gr1"
+                x1="24"
+                x2="24"
+                y1="16.026"
+                y2="18.015"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#912fbd"></stop>
+                <stop offset="1" stop-color="#9332bf"></stop>
+              </linearGradient>
+              <path
+                fill="url(#i9gMV8RPRiXBVRoCh9BlCa_BJkQWseLWhe4_gr1)"
+                d="M41,18H7c-0.552,0-1-0.448-1-1v0c0-0.552,0.448-1,1-1h34c0.552,0,1,0.448,1,1v0	C42,17.552,41.552,18,41,18z"
+              ></path>
+              <linearGradient
+                id="i9gMV8RPRiXBVRoCh9BlCb_BJkQWseLWhe4_gr2"
+                x1="24"
+                x2="24"
+                y1="42.885"
+                y2="10.323"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#912fbd"></stop>
+                <stop offset="1" stop-color="#9332bf"></stop>
+              </linearGradient>
+              <path
+                fill="url(#i9gMV8RPRiXBVRoCh9BlCb_BJkQWseLWhe4_gr2)"
+                d="M39,11v30c0,1.105-0.895,2-2,2H11c-1.105,0-2-0.895-2-2V11H39z"
+              ></path>
+              <linearGradient
+                id="i9gMV8RPRiXBVRoCh9BlCc_BJkQWseLWhe4_gr3"
+                x1="24"
+                x2="24"
+                y1="7.171"
+                y2="14.301"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#c965eb"></stop>
+                <stop offset="1" stop-color="#c767e5"></stop>
+              </linearGradient>
+              <path
+                fill="url(#i9gMV8RPRiXBVRoCh9BlCc_BJkQWseLWhe4_gr3)"
+                d="M8,11v-1c0-1.657,1.343-3,3-3h26c1.657,0,3,1.343,3,3v1H8z"
+              ></path>
+              <linearGradient
+                id="i9gMV8RPRiXBVRoCh9BlCd_BJkQWseLWhe4_gr4"
+                x1="24"
+                x2="24"
+                y1="4.04"
+                y2="7.022"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#ae4cd5"></stop>
+                <stop offset="1" stop-color="#ac4ad5"></stop>
+              </linearGradient>
+              <path
+                fill="url(#i9gMV8RPRiXBVRoCh9BlCd_BJkQWseLWhe4_gr4)"
+                d="M28,4h-8c-1.105,0-2,0.895-2,2v1h12V6C30,4.895,29.105,4,28,4z"
+              ></path>
+              <linearGradient
+                id="i9gMV8RPRiXBVRoCh9BlCe_BJkQWseLWhe4_gr5"
+                x1="15"
+                x2="33"
+                y1="27"
+                y2="27"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0" stop-color="#ae4cd5"></stop>
+                <stop offset="1" stop-color="#ac4ad5"></stop>
+              </linearGradient>
+              <rect
+                width="18"
+                height="32"
+                x="15"
+                y="11"
+                fill="url(#i9gMV8RPRiXBVRoCh9BlCe_BJkQWseLWhe4_gr5)"
+              ></rect>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
